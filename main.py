7b27ebbc5dfb14c4e978c636bae201e6c1b45309
keshan-spec <keshanth.programmer@gmail.com>
2020-03-20 23:23:33 +0530

    Added info table func

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from bs4 import BeautifulSoup
 
 URL = 'https://www.worldometers.info/coronavirus/'
 
+
 def run():
     return BeautifulSoup(requests.get(URL).content, features="html.parser")
+
 
 def get_updates():
     soup = run()
@@ -13,18 +15,29 @@ def get_updates():
     return {"Infected": infect_count[0], "Deaths": infect_count[1], "Recovered": infect_count[2]}
 
 
-def search_country(country='Sri Lanka'):
+def get_info_table():
     soup = run()
     table = soup.find('table', {'id': 'main_table_countries_today'})
-    # get countries
-    countries = []
-    for i in table.findAll('tr'):
-        for j in i.findAll('td'):
-            countries.append(j.findAll(text=True)[0])
-            break
-    # pop the last item , that's just 'total :'
-    countries.pop()
+    tmp = []
+    keys = ['Country', 'Total Cases', 'New Cases', 'Total Deaths', 'New Deaths',
+            'Total Recovered', 'Active Cases', 'Serious', 'Tot']  # the keys of the table
+
+    for tr in table.findAll('tr'):
+        for td in tr.findAll('td'):
+            try:
+                tmp.append(td.find(text=True).strip())
+            except Exception:
+                tmp.append(td.find(text=True))
+                pass
+    tmp.pop()  # pop the last item , that's just 'total :'
+    s, e = 0, 9
+    entries = []
+
+    # Loop through the temp list and make a dict with every 9 items
+    for x in range(len(tmp) // 9):
+        entries.append(dict(zip(keys, tmp[s:e])))
+        s += 9
+        e += 9
 
 
-# print(get_updates())
-search_country()
+get_info_table()
